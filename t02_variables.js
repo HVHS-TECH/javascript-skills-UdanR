a334efc637dc4dc7e593fe9eca13f8fb28babaa4
UdanR <24432ur@hvhs.school.nz>
2026-04-28 23:51:02 +0000

    Apr 28, 2026, 11:51 PM

--- t02_variables.js
+++ t02_variables.js
@@ -4,9 +4,9 @@ Introduction
 ****************************/
 console.log("Running t02_varibales.js")
 console.log("user:"+ username+" -"+" $" + money );
-console. log("Howdie,"+ username)
-console. log("As of" + year + "you are"+ age + "years old")
-console. log("You have" + money + "dollars")
+console. log("Howdie,"+ username);
+console. log("As of" + year + "you are"+ age + "years old");
+console. log("You have" + money + "dollars");
 
 
 // Variables
