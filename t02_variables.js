9cb6f3e8f51a21e973f0ab86d02c6b124205b7a1
UdanR <24432ur@hvhs.school.nz>
2026-04-28 23:25:36 +0000

    Apr 28, 2026, 11:25 PM

--- t02_variables.js
+++ t02_variables.js
@@ -1,1 +1,17 @@
 /* Paste your code from task 1 here */
+/****************************
+Introduction
+****************************/
+console.log("Running t01_introduction.js")
+console.log("Listen Mortals you shall dye infront of the almighty demon lord")
+// Variables
+var username = "udan"
+var money = 10
+
+/****************************
+Main Code
+****************************/
+
+/****************************
+Functions
+****************************/
