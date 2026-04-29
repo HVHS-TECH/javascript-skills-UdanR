f3ddbfaf4812d8d86d8f0f7c0954fa9992e21b36
UdanR <24432ur@hvhs.school.nz>
2026-04-29 00:16:13 +0000

    Apr 29, 2026, 12:16 AM

--- t03_maths.js
+++ t03_maths.js
@@ -10,15 +10,23 @@ var username = "Mr.Legend";
 var age = 22;
 var money = 10;
 var born_age = year;
+var future_age = year;
+var half_money = money
 /****************************
 Main Code
 ****************************/
+console.log(half_money / 2);
 console.log(born_age - age);
-console.log("Running t03_maths.js")
+console.log("Running t03_maths.js");
 console.log("Howdie,"+ username);
 console.log("As of " + year + " you are "+ age + " years old");
 console.log("You were born in " + born_age);
-console.log("You have " + money + " dollars");
+console.log("In 10 years you will be " + future_age + " years old");
+console.log("You have $" + money);
+console.log("You spend half of your money, now you have $" + half_money);
+
+
+
 
 
 
