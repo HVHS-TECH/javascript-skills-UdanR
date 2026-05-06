1fff24bcd81f287ac399ea8520c5213fcd555450
UdanR <24432ur@hvhs.school.nz>
2026-05-06 11:08:43 +0000

    May 6, 2026, 11:08 AM

--- t15_for_loop.js
+++ t15_for_loop.js
@@ -158,8 +158,9 @@ function reply_choco_rating(){
     Chocolate_Rating.innerHTML = ratingArray[rating];
 
 }
+const Chocolate_Rating = document.getElementById ("Chocolate_Rating_Area");
 function Printmilksong() {
     for (var count = Starting_Number; count > 0; count-- ){
-        Lyrics_Area.innerHTML += "<p>"+count+" bottles of milk on the wall<br>"+count+"bottle of milk<br>T</p>"
+        Lyrics_Area.innerHTML += "<p>"+count+" bottles of milk on the wall<br>"+count+"bottle of milk<br>Take one out pass it around</p>"
     }
 }
