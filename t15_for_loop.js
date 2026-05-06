18f86e1ecab219f7de210fa4c8745246b96552ee
UdanR <24432ur@hvhs.school.nz>
2026-05-06 11:17:10 +0000

    May 6, 2026, 11:17 AM

--- t15_for_loop.js
+++ t15_for_loop.js
@@ -162,7 +162,7 @@ function reply_choco_rating(){
 
 const Lyrics_Area = document.getElementById ("Lyrics_Print_Area");
 function Printmilksong() {
-    const Starting_Number = document.getElementById ("Starting_number");
+    const Starting_Number = document.getElementById (Starting_number.value);
     for (var count = Number(Starting_Number); count > -1; count-- ){
         Lyrics_Area.innerHTML += "<p>"+count+" bottles of milk on the wall<br>"+count+"bottle of milk<br>Take one out pass it around</p>"
         console.log (count+" bottles of milk on the wall<br>"+count+"bottle of milk<br>Take one out pass it around")
