6b9eb45938d5b5068bc28fbc326b52eaed7aca20
UdanR <24432ur@hvhs.school.nz>
2026-05-07 01:15:45 +0000

    May 7, 2026, 1:15 AM

--- t19_string_handling.js
+++ t19_string_handling.js
@@ -47,12 +47,6 @@ function getFormInfo (event){
     var username = FORM_NAME.value;
     console.log ("Username value"+username);
     console.log ("Username Lenght"+username.length);
-    if (username.length < 3) {
-        alert("Too short username");
-        event.preventDefault();
-        return;
-    }
-
     const FORM_AGE = document.getElementById ("age_input");
     var age = Number(FORM_AGE.value);
     const FORM_YEAR = document.getElementById ("year_input");
